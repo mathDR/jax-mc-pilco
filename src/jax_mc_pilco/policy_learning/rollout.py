bbb5a9b23ce44312c0cde8aeb92e29eb1a7b4ef1
Daniel Marthaler <dan.marthaler@gmail.com>
2025-04-28 16:58:50 -0400

    trying to get it to work

--- src/jax_mc_pilco/policy_learning/rollout.py
+++ src/jax_mc_pilco/policy_learning/rollout.py
@@ -1,52 +1,164 @@
+""" Optimize a controller on a given cost function."""
+
+import equinox as eqx
+import optax as ox
+import numpy as np
 import jax
+import jax.numpy as jnp
+from jaxtyping import Array, ArrayLike, Float, Int, PyTree
+from typing import Callable, Tuple
 import jax.random as jr
-import typing as tp
-from flax import nnx
-from ..controller import Controller
-
-Model = tp.TypeVar("Model", bound=nnx.Module)
-from jaxtyping import ArrayLike
-from gpjax.typing import (
-    Array,
-    KeyArray,
-    ScalarFloat,
-)
-from typing import Tuple
-
-num_particles = 100
+import gymnasium
 
 
-def one_rollout_step(carry: Tuple, t: ScalarFloat) -> Tuple[Tuple, ScalarFloat]:
-    compute_action, predict_all_outputs, key, samples, total_cost = carry
-    key, *subkeys = jr.split(key, num_particles + 1)
-    u = jax.vmap(compute_action)(
-        samples, jnp.tile(t, num_particles), jnp.array(subkeys)
-    )
-    this_state = jnp.hstack((samples, u))
-    predictive_moments = predict_all_outputs(this_state)
-    key, subkey = jr.split(key)
-    samples = jnp.squeeze(
-        vectorized_sample(
-            key,
-            predictive_moments[:, :, 0],
-            jax.vmap(jnp.diag)(predictive_moments[:, :, 1]),
-            1,
-        )
-    )
-    cost = jnp.sum(jax.vmap(cart_pole_cost)(samples))
-    return (compute_action, predict_all_outputs, key, samples, total_cost + cost), cost
+def remake_state(x):
+    return np.array([x[0], np.sin(x[1]), np.cos(x[1]), x[2], x[3]])
 
 
-def rollout(
-    policy: Controller,
-    model: Model,
-    init_samples: ArrayLike,
+def fit_controller(  # noqa: PLR0913
+    *,
+    policy: eqx.Module,
+    env: gymnasium.wrappers.common.TimeLimit,
+    num_particles: Int,
     timesteps: ArrayLike,
-    key: KeyArray = jr.PRNGKey(42),
-) -> ScalarFloat:
-    action = Partial(policy.compute_action)
-    pao = Partial(model.predict_all_outputs)
-    (action, pao, key, samples, total_cost), result = jax.lax.scan(
-        one_rollout_step, (action, pao, key, init_samples, 0), timesteps
+    gp_model: eqx.Module,
+    obj_func: Callable,
+    optim: ox.GradientTransformation,
+    key: ArrayLike = jr.PRNGKey(42),
+    num_iters: Int = 100,
+    unroll: Int = 5,
+) -> Tuple[eqx.Module, Array]:
+    """The optimization loop for fitting the policy parameters."""
+    # Now do a rollout with this model
+    # Generate an initial state
+    x, _ = env.reset()
+    sample = remake_state(x)
+    key, subkey = jr.split(key)
+    # Generate an initial (random) action
+    u = env.action_space.sample()
+    # initialize some particles
+    initial_particles = gp_model.get_samples(
+        key, jnp.array([sample]), jnp.array([u]), num_particles
     )
-    return total_cost / len(timesteps)
+
+    @eqx.debug.assert_max_traces(max_traces=1)
+    def rollout(
+        policy: eqx.Module,
+        init_samples: ArrayLike,
+        model: eqx.Module,
+        timesteps: ArrayLike,
+        key: ArrayLike = jr.key(123),
+    ) -> Float:
+        policy_params, policy_static = eqx.partition(policy, eqx.is_array)
+
+        def one_rollout_step(
+            carry: Tuple[ArrayLike, ArrayLike, ArrayLike, Float], timestep: Float
+        ) -> Tuple[Tuple[ArrayLike, ArrayLike, ArrayLike, Float], Float]:
+            policy_params, key, samples, total_cost = carry
+            policy = eqx.combine(policy_params, policy_static)
+            actions = jax.vmap(policy)(samples, jnp.tile(timestep, num_particles))
+
+            key, subkey = jr.split(key)
+            samples = model.get_samples(key, samples, actions, 1)
+            cost = jnp.mean(jax.vmap(obj_func)(jnp.hstack((samples, actions))))
+            return (policy_params, key, samples, total_cost + cost), cost
+
+        total_cost = 0
+        (policy_params, key, samples, total_cost), result = jax.lax.scan(
+            one_rollout_step, (policy_params, key, init_samples, total_cost), timesteps
+        )
+        return total_cost
+
+    opt_state = optim.init(eqx.filter(policy, eqx.is_array))
+
+    # Mini-batch random keys to scan over.
+    iter_keys = jr.split(key, num_iters)
+
+    # Optimisation step.
+    @eqx.filter_jit
+    def make_step(
+        policy: eqx.Module,
+        opt_state: PyTree,
+    ) -> Tuple[eqx.Module, PyTree, Float]:
+        loss_value, loss_gradient = eqx.filter_value_and_grad(rollout)(
+            policy, initial_particles, gp_model, timesteps
+        )
+        updates, opt_state = optim.update(
+            loss_gradient, opt_state, eqx.filter(policy, eqx.is_array)
+        )
+        policy = eqx.apply_updates(policy, updates)
+        return policy, opt_state, loss_value
+
+    # Optimisation loop - hack an early stopping criteron
+    best_loss: Float = float("inf")
+    patience: int = 5  # Number of steps of no improvement before stopping
+    patience_count: int = 0  # Number of steps since last improving update.
+    min_delta: Float = (
+        1e-3  # Minimum delta between updates to be considered an improvement
+    )
+    losses = []
+    ## Need to apply the dropout schedule...
+    for step in range(num_iters):
+        policy, opt_state, train_loss = make_step(policy, opt_state)
+        losses.append(train_loss)
+        # patience_count = jax.lax.select(
+        #     best_loss - train_loss > min_delta, 0, patience_count + 1
+        # )
+        # best_loss = jax.lax.select(train_loss < best_loss, train_loss, best_loss)
+        # print(f"{step=}, train_loss={train_loss.item()}, best_loss={best_loss.item()}, ")
+        # if patience_count > patience:
+        #     print(f"Terminating due to early stopping at {step=}, train_loss={train_loss.item()}, ")
+        #     break
+        if (step % 100) == 0 or (step == num_iters - 1):
+            print(f"{step=}, train_loss={train_loss.item()}, ")
+
+    return policy, jnp.array(losses)
+
+
+def rollout_check(
+    policy: eqx.Module,
+    env: gymnasium.wrappers.common.TimeLimit,
+    num_particles: Int,
+    model: eqx.Module,
+    obj_func: Callable,
+    timesteps: ArrayLike,
+    key: ArrayLike = jr.key(123),
+) -> Tuple[Float, Array]:
+    policy_params, policy_static = eqx.partition(policy, eqx.is_array)
+    # Now do a rollout with this model
+    # Generate an initial state
+    x, _ = env.reset()
+    key, subkey = jr.split(key)
+    # Generate an initial (random) action
+    u = env.action_space.sample()
+    # initialize some particles
+    init_samples = model.get_samples(key, jnp.array([x]), jnp.array([u]), num_particles)
+
+    def one_rollout_step(
+        carry: Tuple[ArrayLike, ArrayLike, List[ArrayLike], ArrayLike, Float],
+        timestep: Float,
+    ) -> Tuple[Tuple[ArrayLike, ArrayLike, List[ArrayLike], ArrayLike, Float], Float]:
+        policy_params, key, all_samples, samples, total_cost = carry
+        policy = eqx.combine(policy_params, policy_static)
+        actions = jax.vmap(policy)(samples, jnp.tile(timestep, num_particles))
+
+        key, subkey = jr.split(key)
+        samples = model.get_samples(key, samples, actions, 1)
+        all_samples.append(samples)
+        cost = jnp.mean(jax.vmap(obj_func)(jnp.hstack((samples, actions))))
+        return (
+            policy_params,
+            key,
+            all_samples,
+            samples,
+            total_cost + cost,
+        ), cost
+
+    total_cost = 0
+    all_samples = list(init_samples)
+    (policy_params, key, all_samples, samples, total_cost), result = jax.lax.scan(
+        one_rollout_step,
+        (policy_params, key, all_samples, init_samples, total_cost),
+        timesteps,
+    )
+    return total_cost, jnp.array(all_samples)
--- src/jax_mc_pilco/rewards.py
+++ src/jax_mc_pilco/rewards.py
@@ -1,73 +1,33 @@
-from flax import nnx
 import jax.numpy as jnp
 from jax import Array
 from jax.typing import ArrayLike
-from typing import Tuple
 
 
-class ExponentialReward(nnx.Module):
+def cart_pole_cost(
+    states_sequence: ArrayLike,
+    target_state: ArrayLike = jnp.array(
+        [
+            0.0,
+            0.0,
+        ]
+    ),
+    lengthscales: ArrayLike = jnp.array([3.0, 1.0]),
+) -> Array:
     """
-    Compute expectation and variance, and their derivatives of an exponentiated negative quadratic cost
-    exp( -(x-z).T * W * (x-z)/2 )
-    where x ~ N(m,S)
+    Cost function given by the combination of the saturated distance between |theta| and 'target angle', and between x and 'target position'.
     """
+    cart_pos = states_sequence[0]
+    sin_pole_angle = states_sequence[1]
+    cos_pole_angle = states_sequence[2]
+    cart_velocity = states_sequence[3]
+    angle_velocity = states_sequence[4]
+    target_theta = 0.0
+    target_cart_velocity = 0.0
+    target_theta_dot = 0.0
 
-    def __init__(
-        self,
-        state_dim: int,
-        weight_matrix: ArrayLike = None,
-        target_state: ArrayLike = None,
-    ):
-        self.state_dim = state_dim
-        if weight_matrix is not None:
-            self.weight_matrix = weight_matrix.reshape(state_dim, state_dim)
-        else:
-            self.weight_matrix = jnp.eye(state_dim)
-
-        if target_state is not None:
-            self.target_state = target_state.reshape(1, state_dim)
-        else:
-            self.target_state = jnp.zeros((1, state_dim))
-
-    def compute_reward(
-        self, state_mean: ArrayLike, state_covariance: ArrayLike
-    ) -> Tuple[Array, Array]:
-        """
-        Reward function, calculating mean and variance of rewards, given
-        mean and variance of state distribution, along with the target State
-        and a weight matrix.
-        Input state_mean : [self.state_dim ,1]
-        Input state_covariance : [self.state_dim, self.state_dim]
-
-        Output expected_reward : [1, 1]
-        Output reward_variance : [1, 1]
-        """
-        mu = state_mean - self.target_state
-        eye_state_dim = jnp.eye(self.state_dim)
-
-        State_Covar_times_Weight = jnp.dot(state_covariance, self.weight_matrix)
-
-        iSpW = jnp.transpose(
-            jnp.linalg.solve(
-                (eye_state_dim + State_Covar_times_Weight),
-                jnp.transpose(self.weight_matrix),
-            )
-        )
-
-        expected_reward = jnp.exp(
-            -0.5 * jnp.dot(mu, jnp.dot(iSpW, jnp.transpose(mu)))
-        ) / jnp.sqrt(jnp.linalg.det(eye_state_dim + State_Covar_times_Weight))
-
-        i2SpW = jnp.transpose(
-            jnp.linalg.solve(
-                (eye_state_dim + 2 * State_Covar_times_Weight),
-                jnp.transpose(self.weight_matrix),
-            )
-        )
-
-        r2 = jnp.exp(-jnp.dot(mu, jnp.dot(i2SpW, jnp.transpose(mu)))) / jnp.sqrt(
-            jnp.linalg.det(eye_state_dim + 2 * State_Covar_times_Weight)
-        )
-
-        reward_variance = r2 - jnp.dot(state_mean, state_mean)
-        return expected_reward, reward_variance
+    return 1 - jnp.exp(
+        -(jnp.square((sin_pole_angle - target_theta) / lengthscales[0]))
+        - (jnp.square((cos_pole_angle - 1.0) / lengthscales[0]))
+        - (jnp.square((angle_velocity - target_theta_dot) / lengthscales[1]))
+        - (jnp.square((cart_velocity - target_cart_velocity) / lengthscales[1]))
+    )
