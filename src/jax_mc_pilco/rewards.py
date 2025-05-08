32d78a9d7cc49dabd80bd1dad0a64d0c5947ba0c
Daniel Marthaler <dan.marthaler@gmail.com>
2025-05-08 14:00:26 -0400

    reran notebook

--- src/jax_mc_pilco/rewards.py
+++ src/jax_mc_pilco/rewards.py
@@ -16,18 +16,55 @@ def cart_pole_cost(
     """
     Cost function given by the combination of the saturated distance between |theta| and 'target angle', and between x and 'target position'.
     """
+    # cart_pos = states_sequence[0]
+    # sin_pole_angle = states_sequence[1]
+    # cos_pole_angle = states_sequence[2]
+    # cart_velocity = states_sequence[3]
+    # angle_velocity = states_sequence[4]
     cart_pos = states_sequence[0]
-    sin_pole_angle = states_sequence[1]
-    cos_pole_angle = states_sequence[2]
-    cart_velocity = states_sequence[3]
-    angle_velocity = states_sequence[4]
+    pole_angle = states_sequence[1]
+    cart_velocity = states_sequence[2]
+    angle_velocity = states_sequence[3]
     target_theta = 0.0
     target_cart_velocity = 0.0
     target_theta_dot = 0.0
 
+    # return 1 - jnp.exp(
+    #     -(jnp.square((sin_pole_angle - target_theta) / lengthscales[0]))
+    #     - (jnp.square((cos_pole_angle - 1.0) / lengthscales[0]))
+    #     - (jnp.square((angle_velocity - target_theta_dot) / lengthscales[1]))
+    #     - (jnp.square((cart_velocity - target_cart_velocity) / lengthscales[1]))
+    # )
     return 1 - jnp.exp(
-        -(jnp.square((sin_pole_angle - target_theta) / lengthscales[0]))
-        - (jnp.square((cos_pole_angle - 1.0) / lengthscales[0]))
+        -(jnp.square((pole_angle - target_theta) / lengthscales[0]))
         - (jnp.square((angle_velocity - target_theta_dot) / lengthscales[1]))
         - (jnp.square((cart_velocity - target_cart_velocity) / lengthscales[1]))
+    )
+
+
+def pendulum_cost(
+    states_sequence: ArrayLike,
+    target_state: ArrayLike = jnp.array(
+        [
+            1.0,
+            0.0,
+            0.0,
+        ]
+    ),
+    lengthscales: ArrayLike = jnp.array([3.0, 3.0, 1.0]),
+) -> Array:
+    """
+    Cost function given by the combination of the saturated distance between |theta| and 'target angle'.
+    """
+
+    cos_pole_angle = states_sequence[0]
+    sin_pole_angle = states_sequence[1]
+    angle_velocity = states_sequence[2]
+    target_theta = 0.0
+    target_theta_dot = target_state[2]
+
+    return 1 - jnp.exp(
+        -(jnp.square((sin_pole_angle - jnp.sin(target_theta)) / lengthscales[0]))
+        - (jnp.square((angle_velocity - target_theta_dot) / lengthscales[1]))
+        - (jnp.square((sin_pole_angle - jnp.cos(target_theta)) / lengthscales[0]))
     )
