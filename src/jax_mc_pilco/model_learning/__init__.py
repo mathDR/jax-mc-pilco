f3c8047306422bd2072b259208ea0b40f4c3ce1b
Daniel Marthaler <dan.marthaler@gmail.com>
2025-09-10 19:31:01 -0600

    more files

--- src/jax_mc_pilco/model_learning/__init__.py
+++
@@ -1,9 +0,0 @@
-from jax_mc_pilco.model_learning.gp.gaussian_process import (
-    GaussianProcess as GaussianProcess,
-)
-from jax_mc_pilco.model_learning.gp import (
-    kernels as kernels,
-    noise as noise,
-    solver as solver,
-    transforms as transforms,
-)
